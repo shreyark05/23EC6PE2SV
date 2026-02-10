c70cf6a0db0109f5457015de8f369591d59619e0
shreyark05 <rkshreya05@gmail.com>
2026-02-10 22:09:36 +0530

    Add files via upload

---
+++ AAT1/EXP3/testbench.sv
@@ -0,0 +1,33 @@
+
+module tb;
+
+    logic [3:0] in;
+    logic [1:0] out;
+    logic valid;
+
+    priority_enc dut (.*);
+
+    covergroup cg_enc;
+        cp_in : coverpoint in {
+            bins b0 = {1};
+            bins b1 = {2};
+            bins b2 = {4};
+            bins b3 = {8};
+            bins others = default;
+        }
+    endgroup
+
+    cg_enc cg = new();
+
+    initial begin
+        repeat (50) begin
+            in = $urandom_range(0,15);
+            #5;
+            cg.sample();
+        end
+
+        $display("Coverage : %0.2f %%", cg.get_inst_coverage());
+        $finish;
+    end
+
+endmodule
