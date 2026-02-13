e10fa768c2c9e2232519b18fceb91114db653860
shreyark05 <rkshreya05@gmail.com>
2026-02-13 20:25:46 +0530

    Delete CADENCE/D_inter.sv

--- CADENCE/D_inter.sv
+++
@@ -1,8 +0,0 @@
-interface D_inter(input bit clk);
-  logic d, reset, set;
-  logic q;
-
-  
-  modport RTL1(input clk, d, reset, set, output q);
-  modport test1(input q, output d, reset, set);
-endinterface: D_inter
