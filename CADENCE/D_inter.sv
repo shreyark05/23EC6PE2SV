a65f6adffec713ac4bc730c8da6172c76e151987
shreyark05 <rkshreya05@gmail.com>
2026-02-13 20:21:04 +0530

    Add modports to D_inter interface

--- CADENCE/D_inter.sv
+++ CADENCE/D_inter.sv
@@ -1,3 +1,8 @@
 interface D_inter(input bit clk);
   logic d, reset, set;
   logic q;
+
+  
+  modport RTL1(input clk, d, reset, set, output q);
+  modport test1(input q, output d, reset, set);
+endinterface: D_inter
